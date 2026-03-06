c0622eaaf5b8c6892d24be586618807acac441de
Michel Ruwishka <michelruwishka@gmail.com>
2026-03-07 02:27:10 +0530

    reupload the missing part when commiting the last time

--- src/components/ContactsSection.tsx
+++ src/components/ContactsSection.tsx
@@ -27,10 +27,10 @@ export default function ContactsSection() {
                     <h3 className={styles.boxTitle}>Message me here</h3>
                     <div className={styles.contactMethods}>
                         <a href="mailto:elias@elias-dev.ml" className={styles.contactItem}>
-                            <span className={styles.icon}>@</span> elias@elias-dev.ml
+                            <span className={styles.icon}>@</span> michelruwishka@gmail.com
                         </a>
                         <a href="#" className={styles.contactItem}>
-                            <span className={styles.icon}>#</span> Elias#4321
+                            <span className={styles.icon}>#</span> michel0468
                         </a>
                     </div>
                 </div>
