90ffca3ecfe9850c893f996bffaf9b10d3c036b8
Michel Ruwishka <michelruwishka@gmail.com>
2026-03-28 12:09:06 +0530

    Fix homepage caching issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import SkillsSection from "@/components/SkillsSection";
 import AboutSection from "@/components/AboutSection";
 import ContactsSection from "@/components/ContactsSection";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <main>
